bf2a0ad5bea4d4ced42714d02c224b46858d5744
hpham474 <hpham474@gmail.com>
2024-07-24 01:28:43 -0500

    build node class

---
+++ Node.mjs
@@ -0,0 +1,28 @@
+class Node {
+  value;
+  left;
+  right;
+  constructor() {
+    value = null;
+    left = null;
+    right = null;
+  }
+  set value(value) {
+    this.value = value;
+  }
+  set left(node) {
+    this.left = node;
+  }
+  set right(node) {
+    this.right = node;
+  }
+  get value() {
+    return this.value;
+  }
+  get left() {
+    return this.left;
+  }
+  get right() {
+    return this.right;
+  }
+}
